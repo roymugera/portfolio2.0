db5508e09584b4a35ac0452db023c13e19933fd2
Roy Mugera <69948625+Omwami93@users.noreply.github.com>
2021-03-22 20:54:44 +0000

    modified js

---
+++ main.js
@@ -0,0 +1,13 @@
+let webLink = document.getElementById("weblink");
+let gitHubLink = document.getElementById("gitHubLink");
+
+
+
+/* 
+
+function rotateImageRight() {
+    let imageCardRight= document.querySelector(".card");
+    imageCardRight.style.transform = 'rotateY(180deg)';
+}; 
+
+*/
